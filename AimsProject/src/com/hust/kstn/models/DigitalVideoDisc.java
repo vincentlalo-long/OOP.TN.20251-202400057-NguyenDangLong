219c37a339600a7e3895eed977670d4c095d23c9
vincentlalo-long <nickmoi3056@gmail.com>
2025-11-15 23:48:41 +0700

    Update classifier and instance member about management id of dvd

--- AimsProject/src/com/hust/kstn/models/DigitalVideoDisc.java
+++ AimsProject/src/com/hust/kstn/models/DigitalVideoDisc.java
@@ -7,6 +7,8 @@ public class DigitalVideoDisc {
 	private String director;
 	private int length;
 	private double cost;
+	private static int nbDigitalVideoDiscs = 0;
+	private int id;
 	//constructor  DVD all attribute  
 	public DigitalVideoDisc(String tittle, String category, String director , int length , double cost) {
 		this.title=tittle;
@@ -14,6 +16,8 @@ public class DigitalVideoDisc {
 		this.director=director;
 		this.length=length;
 		this.cost=cost;
+		nbDigitalVideoDiscs ++;
+		this.id = nbDigitalVideoDiscs ;
 	}
 	public String getCategory() {
 		return category;
@@ -33,12 +37,16 @@ public class DigitalVideoDisc {
 	//constructor  DVD  with title
 	public DigitalVideoDisc(String title) {
 		this.title=title;
+		nbDigitalVideoDiscs++;
+	    this.id = nbDigitalVideoDiscs;
 	}
 		//constructor DVD category , cost , title
 	public DigitalVideoDisc(String category, String title , double cost) {
 		this.title=title;
 		this.category=category;
 		this.cost=cost;
+		nbDigitalVideoDiscs++;
+	    this.id = nbDigitalVideoDiscs;
 	}
 		//constructor DVD title , category , director , cost
 	public DigitalVideoDisc(String director , String category , String title , double cost) {
@@ -46,5 +54,13 @@ public class DigitalVideoDisc {
 		this.category=category;
 		this.director=director;
 		this.cost=cost;
+		nbDigitalVideoDiscs++;
+	    this.id = nbDigitalVideoDiscs;
+	}
+	public int getId() {
+		return id;
+	}
+	public static int getNbDigitalVideoDiscs() {
+		return nbDigitalVideoDiscs;
 	}
 }
