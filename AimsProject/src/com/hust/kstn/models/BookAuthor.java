1d525701596fe077c5075d6cf4e7843ceb7891dc
vincentlalo-long <nickmoi3056@gmail.com>
2025-11-17 00:48:25 +0700

    Update BookAuthor.java due to unsaved

--- AimsProject/src/com/hust/kstn/models/BookAuthor.java
+++ AimsProject/src/com/hust/kstn/models/BookAuthor.java
@@ -1,5 +1,33 @@
 package com.hust.kstn.models;
 
-public class BookAuthor {
 
+public class BookAuthor {
+    private String fullName;
+    private int yearOfBirth;
+    private String shortBiography;
+
+    public BookAuthor(String fullName, int yearOfBirth, String shortBiography) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+        this.shortBiography = shortBiography;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public String getShortBioGraphy() {
+        return shortBiography;
+    }
+
+    @Override
+    public String toString() {
+        return this.fullName + " ("
+            + this.yearOfBirth + ", "
+            + this.shortBiography + ")\n";
+    }
 }
